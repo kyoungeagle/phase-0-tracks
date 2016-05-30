0c2b123d3bf9f3a509509c3786d50e03b17b08bb
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-05-29 22:56:36 -0700

    Final version of Release 4.4 with testing done

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,31 +1,30 @@
 # Solo Challenge 4.4: Control Flow Practice
 
 puts "How many employees will be processed?"
-employees_processed = gets.chomp
-employees_processed = employees_processed.to_i
-employees_processed_counter = 0
+employees = gets.chomp
+employees = employees.to_i
+employees_counter = 0
 
-while employees_processed > employees_processed_counter
+while employees > employees_counter
 	puts "What's your name?"
 	name = gets.chomp
-	name.capitalize
 
 	puts "How old are you?"
-	age_year = gets.chomp
+	age = gets.chomp
 	#make users response an integer
-	age_year = age_year.to_i
+	age = age.to_i
 
 	puts "In what year were you born?"
-	year_of_birth = gets.chomp
+	birth_year = gets.chomp
 	#make users response an integer
-	year_of_birth = year_of_birth.to_i
+	birth_year = birth_year.to_i
 
 	garlic_bread_counter = 0
 	#create feedback loop for answer other than Y/N
 	while garlic_bread_counter < 1
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	garlic_bread = gets.chomp
-	if garlic_bread.downcase == "yes"
+		puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+		garlic_bread = gets.chomp
+		if garlic_bread.downcase == "yes"
 			garlic_bread = true
 			garlic_bread_counter += 1
 		elsif garlic_bread.downcase == "no"
@@ -35,27 +34,30 @@ while employees_processed > employees_processed_counter
 			puts "I don't understand. Please enter a valid response."
 		end 
 	end 
-	health_insurance = 0
+
+
+	healthinsurance_counter = 0
 	#create feedback loop for answer other than Y/N
-	while health_insurance < 1
+	while healthinsurance_counter < 1
 		puts "Would you like to enroll in the company’s health insurance?"
-		health_insurance = gets.chomp
-		if health_insurance.downcase == "yes"
-			health_insurance = true
-			health_insurance_counter += 1
-		elsif health_insurance.downcase == "no"
-			health_insurance = false
-			health_insurance_counter += 1
+		healthinsurance = gets.chomp
+		if healthinsurance.downcase == "yes"
+			healthinsurance = true
+			healthinsurance_counter += 1
+		elsif healthinsurance.downcase == "no"
+			healthinsurance = false
+			healthinsurance_counter += 1
 		else
 			puts "Invalid response. Please reenter your preference to enroll in the company's health insurance."
 		end
 	end
 
 	current_year = Time.new.year
+
 	allergies_counter = 0
 	puts "If you have any allergies, list them one at a time. If you don't have allergies, please type 'none'. Indicate that you are finished by typing 'done'."
 	while allergies_counter < 1
-		puts "Please list your allergy:"
+		puts "Please list your allergy."
 		allergies = gets.chomp
 		if allergies.downcase == "sunshine"
 			allergies_counter +=1
@@ -68,28 +70,34 @@ while employees_processed > employees_processed_counter
 		end
 	end 
 
-	elsif ((current_year - year_of_birth) == age) && (garlic_bread && health_insurance)
-		puts "Probably not a vampire."
-		employees_processed_counter += 1
-	elsif ((current_year - year_of_birth) != age) && (garlic_bread || health_insurance)
+	if allergies == "sunshine"
 		puts "Probably a vampire."
-		employees_processed_counter += 1
-	elsif ((current_year - year_of_birth) != age) && (!garlic_bread && !health_insurance)
-		puts "Almost certainly a vampire."
-		employees_processed_counter += 1
+		employees_counter += 1
+
 	elsif (name == "Drake Cula") || (name == "Tu Fang")
 		puts "Definitely a vampire!"
-		employees_processed_counter += 1
-	if allergy == "sunshine"
+		employees_counter += 1
+
+	elsif ((current_year - birth_year) == age) && (garlic_bread && healthinsurance)
+		puts "Probably not a vampire."
+		employees_counter += 1
+
+	elsif ((current_year - birth_year) != age) && (garlic_bread || healthinsurance)
 		puts "Probably a vampire."
-		employees_processed_counter += 1
+		employees_counter += 1
+
+	elsif ((current_year - birth_year) != age) && (!garlic_bread && !healthinsurance)
+		puts "Almost certainly a vampire."
+		employees_counter += 1
+
 	else
 		puts "Results inconclusive."
-		employees_processed_counter += 1
+		employees_counter += 1
 	end
 end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
 
 
 
