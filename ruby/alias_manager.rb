54c7a02712e7a9bc6352ebdf610119e116ce5ed2
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-05 12:02:33 -0700

    Initial commit

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,21 @@
+# 5.6 Solo Challenge: Manipulating Strings with Iteration
+
+# Psuedocode of a method that takes a spy's real name and creates a fake name 
+
+#Release 0: Part 1
+
+#1. Have user type in first and last name
+#2. Determine alphabetical characters in first and last name
+#3. Make all letters lowercase
+#4. Split the first and last names (strings) into arrays
+#5. Of that list of characters, determine which characters are vowels (a, e, i, o, u) and which are considered #consonants
+#6. Change all vowels and consonants to become the #following character, respectively (i.e. 'a' becomes 'e,' #'e' becomes 'o', 'f' becomes 'g', etc.) by advancing a letter by one 
+#7. Swap first and last name
+#8. Present spy with fake name
+
+#Release 0: Part 2
+
+#Provide a user interface that lets a user enter a name and get a fake name back. Let the user do this repeatedly until they decide to quit by typing 'quit'. 
+
+#User/secret agent types in first and last name
+
