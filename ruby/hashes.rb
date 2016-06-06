2e1c6b7e918248ca8be17b5d8ee11bed839593be
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-05 20:41:08 -0700

    second commit

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -13,3 +13,106 @@
 
 #Prompt the designer/user for all of this information.
 
+client_info = {}
+
+puts "Client's name?"
+client_info[:name] = gets.chomp
+
+puts "Clients age?"
+client_info["age"] = gets.chomp
+
+puts "How many pets does the client own?"
+client_info["number_of_pets"] = gets.chomp
+
+puts "How many kids does the client have?"
+client_info["number_of_kids"] = gets.chomp
+
+puts "What decor theme does the client prefer?"
+client_info[:decor] = gets.chomp
+
+client_info[:designer_rookie] = ""
+
+	loop do
+
+	puts "Is this your first time working with an interior designer? (Yes/No)"
+	client_info[:designer_rookie] = gets.chomp
+	if client_info[:designer_rookie] == "Yes" || client_info [:designer_rookie] == "No"
+		break
+	end
+end
+
+if 	client_info[:designer_rookie] == "Yes"
+	client_info[:designer_rookie] = true
+else
+	client_info[:designer_rookie] = false
+end
+
+#Print the hash back out to the screen when the designer has answered all of the questions.
+puts "Client Info Summary"
+puts "Name: " << client_info[:name]
+puts "Age: " << client_info["age"]
+puts "Pets? " << client_info["number_of_pets"]
+puts "Kids? " << client_info["number_of_kids"]
+puts "Decor Preference:" << client_info[:decor]
+if client_info[:designer_rookie] == true
+	puts "Client is new to this industry. Be nice."
+else
+	puts "Client has worked with an interior designer before. Go wild."
+end
+
+#Give the user the opportunity to update a key 
+puts " "
+puts "Thanks for your information. Please review. Do you need to make any changes before you submit the information? (Yes/No)"
+update = gets.chomp
+
+if update == 'Yes'
+	puts "Which field do you need to update?"
+	puts "Name, Age, Pets, Children, Decor, or Designer Experience"
+	update = gets.chomp
+	puts "Please enter your updated information in the appropriate field."
+	new_info = gets.chomp
+
+# Update the hash according to chosen field.
+	case update
+	when "name"
+	client_info [:name] = new_value
+	when "age"
+	client_info ["age"] = new_value
+	when "pets"
+	client_info ["number_of_pets"] = new_value
+	when "kids"
+	client_info ["number_of_kids"] = new_value
+	when "Decor"
+	client_info [:decor] = new_value
+	when "check"
+	client_info[:designer_rookie] = new_value
+	
+	if client_info[:designer_rookie] == "Yes"
+	puts "Client is new to this industry. Be nice."
+   else
+	puts "Client has worked with an interior designer before. Go wild."
+end
+	else
+	puts "No changes made. Your entry is invalid."
+end
+	
+#Print the hash back out to the screen when the designer has answered all of the questions.
+
+puts " "
+puts "Client Info Summary"
+puts "Name: " << client_info[:name]
+puts "Age: " << client_info["age"]
+puts "Pets? " << client_info["number_of_pets"]
+puts "Kids? " << client_info["number_of_kids"]
+puts "Decor Preference:" << client_info[:decor]
+
+if client_info[:designer_rookie] == true
+	puts "Client is new to this industry. Be nice."
+else
+	puts "Client has worked with an interior designer before. Go wild."
+end
+
+#Print the latest version of the hash, and exit the program.
+else 
+	puts "Update complete. You may close the window."
+end
