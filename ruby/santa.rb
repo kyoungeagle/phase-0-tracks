d0a52a8bb94f07e344cf89d55113bd46b01507fe
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-07 22:24:01 -0700

    Release 1 added

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,22 +1,34 @@
 class Santa
-		def speak
-			puts "Ho, ho, ho! Haaaapy Holidays!"
-		end
 
-		def eat_milk_and (cookie)
-			puts "That was a good #{cookie}!"
-		end
-
-		def initialize
+	def initialize(gender, ethnicity)
 			@gender = gender
 			@ethnicity = ethnicity
-			@reindeer-ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+			@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 			@age = 0
-			puts "Initializing Santa Instance"
-		end
+	end
+
+	def speak
+			puts "Ho, ho, ho! Haaaapy Holidays!"
+	end
+
+	def eat_milk_and_cookies(cookie)
+			puts "That was a good #{cookie}!"
+	end
+	def identity
+			puts "I identify as #{@ethnicity} and #{@gender}"
+	end		
 end
 
+
+santas = []
+genders = ["cisgender", "pangender", "transgender", "male", "female", "two-spirit"]
+ethnicities = ["black", "latino", "white", "Native American", "prefer not to say", "Mystical Creature (red panda)", "N/A"]
+genders.length.times do |iteration|
+	santas << Santa.new(genders[iteration], ethnicities[iteration])
+
+
+
 santa = Santa.new
-santa.speak 
-santa.eat_milk_and_cookies("sugar cookie")	
+#santa.speak 
+#santa.eat_milk_and_cookies("sugar cookie")	
 
