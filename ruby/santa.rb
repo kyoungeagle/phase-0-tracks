7ba18de27e37a817fba36f4a178385d76693c5ae
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-07 23:07:49 -0700

    Work from release 3 updated

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+	#?attr_reader :ethnicity, :age
+	#?attr_accessor :age
 
 	def initialize(gender, ethnicity)
 			@gender = gender
@@ -33,27 +35,28 @@ class Santa
 		@reindeer_ranking.insert(-1, reindeer)
 	end
 #getter methods
-	def ethnicity
-		@ethnicity
-	end	
+	#def ethnicity
+	#	@ethnicity
+	#end	
 
-	def age
-		@age
-	end
+	#def age
+	#	@age
+	#end
 
 end
 
 
 genders = ["cisgender", "pangender", "transgender", "male", "female", "two-spirit"]
 ethnicities = ["black", "latino", "white", "Native American", "prefer not to say", "Mystical Creature (red panda)", "N/A"]
-#genders.length.times do |iteration|
-#santas << Santa.new(genders[iteration], ethnicities[iteration])
+genders.length.times do |iteration|
+santas << Santa.new(genders[iteration], ethnicities[iteration])
 
 
 santas = []
 
 santa = Santa.new("","")
-puts "#{get_mad_at.reindeer}"
-#santa.speak 
-#santa.eat_milk_and_cookies("sugar cookie")	
+#santa.speak
+#santa.eat_milk_and_cookies("sugar cookie")
+#santa.celebrate_birthday
+#p santa.age
 
