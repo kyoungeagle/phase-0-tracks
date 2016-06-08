34caa82066da9e7d1c6be2ceeaf3a0f6437c9a62
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-07 22:46:45 -0700

    Release 3 work completed

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -14,21 +14,46 @@ class Santa
 	def eat_milk_and_cookies(cookie)
 			puts "That was a good #{cookie}!"
 	end
+
 	def identity
 			puts "I identify as #{@ethnicity} and #{@gender}"
-	end		
+	end	
+
+	def celebrate_birthday
+		@age += 1
+	end
+#setter method
+	def gender=(new_gender)
+		@gender = new_gender
+		puts @gender
+		
+	end
+	def get_mad_at(reindeer)
+		@reindeer_ranking.delete(reindeer)
+		@reindeer_ranking.insert(-1, reindeer)
+	end
+#getter methods
+	def ethnicity
+		@ethnicity
+	end	
+
+	def age
+		@age
+	end
+
 end
 
 
-santas = []
 genders = ["cisgender", "pangender", "transgender", "male", "female", "two-spirit"]
 ethnicities = ["black", "latino", "white", "Native American", "prefer not to say", "Mystical Creature (red panda)", "N/A"]
-genders.length.times do |iteration|
-	santas << Santa.new(genders[iteration], ethnicities[iteration])
+#genders.length.times do |iteration|
+#santas << Santa.new(genders[iteration], ethnicities[iteration])
 
 
+santas = []
 
-santa = Santa.new
+santa = Santa.new("","")
+puts "#{get_mad_at.reindeer}"
 #santa.speak 
 #santa.eat_milk_and_cookies("sugar cookie")	
 
