e345ab50f5cefffef55df9c5265ae4702daafe0c
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-09 19:51:06 -0700

    release 2 complete

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -38,4 +38,33 @@ puppy.roll_over
 puts puppy.dog_years(49)
 puppy.shake
 
+#Release #2: Designing Jim & Karia's class
 
+class Thespian
+
+	def initialize
+		puts "Initializing new Thespian instance..."
+	end
+
+	def pitch(rate)
+		#rating is on a scale of 1-10
+		puts "This person's pitch is #{rate}!"
+	end
+
+	def experience(musicals)
+		#each musical is worth 3 points
+		puts "Their musical score is #{musicals*3}."
+	end
+
+	def dance(style)
+		#style does not count towards points
+		puts "This person's dance style is #{style}."
+	end
+
+	def end_of_show(bow_or_curtsey)
+		bow_or_curtsey.times do
+		puts "*bows to crowd*"
+		end
+  	end
+
+end
