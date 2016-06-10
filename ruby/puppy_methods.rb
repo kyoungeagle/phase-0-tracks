7369bdab13bb28ba2ca804641132adecf2a15af2
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-09 19:50:47 -0700

    second commit with release 0 and 1 complete

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,10 +2,40 @@
 
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+
+  	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+  	end
+
+  	def speak(integer)
+  		integer.times do 
+  		puts "Woof!" 
+  		end
+  	end
+
+  	def roll_over
+  		puts "*rolls over*"	
+  	end
+
+  	def dog_years(years)
+  		years/7
+  	end
+
+  	def shake
+  		puts "*paw lifts* 'good boy!'"
+  	end
 
 end
+
+puppy = Puppy.new
+puppy.fetch('ball')
+puppy.speak(3)
+puppy.roll_over
+puts puppy.dog_years(49)
+puppy.shake
+
 
