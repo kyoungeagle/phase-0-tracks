35a53314f616aa1c4bf123b696c522bed2b307e9
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-10 11:16:59 -0700

    added flight file

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,14 @@
+#Release 0 
+
 module Shout
-	#we'll put some methods here soon
+	def self.yell_angrily(words)
+		words + "!!!" + " :( "
+	end
+
+	def self.yelling_happily(words)
+		words + "!!!" + " :) "
+	end
 end
 
+p Shout.yell_angrily("dammit")
+p Shout.yelling_happily("I completed Phase 0")
