13d31a2334a97aec91c3a291ecd6d94b278c8acd
Karia Young-Eagle <kariayoung-eagle@Karias-MacBook-Air-2.local>
2016-06-10 11:35:07 -0700

    Updated shout.rb file, completed flight.rb file and made it 50% through release III

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,5 @@
 #Release 0 
-
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :( "
@@ -12,3 +12,32 @@ end
 
 p Shout.yell_angrily("dammit")
 p Shout.yelling_happily("I completed Phase 0")
+=end
+
+#RELEASE III
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " >:( "
+	end
+
+	def yelling_happily(words)
+		words + "!!!" + " :) "
+	end
+end
+
+class Loudspeaker
+	include Shout
+end
+
+class Mom
+	include Shout
+end
+
+loudspeaker = Loudspeaker.new
+p loudspeaker.yell_angrily("EVACTUATE IMMEDIATELY")
+p loudspeaker.yelling_happily("And he scores")
+
+mr_mom = Mom.new
+p mr_mom.yell_angrily("put your clothes away")
+p mr_mom.yelling_happily("So glad you are home")
