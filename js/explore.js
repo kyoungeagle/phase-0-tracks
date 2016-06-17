83dbe1b915ce48f36641c44668a5f2604743a910
Karia Young-Eagle <karia.aye@gmail.com>
2016-06-16 20:08:36 -0700

    commit explore.js file second time

---
+++ js/explore.js
@@ -0,0 +1,23 @@
+// Week VII: Release 4: Write a JavaScript Program
+
+// Pseudocode //
+
+//1. User inputs string of characters to form a word or phrase.
+//2. Convert to a character array
+//3. Combine into one string
+//3. Print new string (characters in reverse)
+//4. If 'hello world' is 'hello world' then print in reverse
+
+function reverse(str) 
+{
+  return str.split('').reverse().join('');
+}
+
+answer = reverse("wow phase 0 is almost over!");
+
+if ("hello world" == "hello world")
+
+{
+	console.log(answer)
+}
+
