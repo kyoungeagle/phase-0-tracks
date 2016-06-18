6ccbfd66efe23b5e9eadaee9337a6336aadfa43b
Karia Young-Eagle <karia.aye@gmail.com>
2016-06-18 11:58:00 -0700

    Finish Release 0 and Release 1

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,5 +3,29 @@
 
 // Declaring two arrays
 
-var colors = ['yellow', 'violet', 'green', 'blue']
-var names = ['Chip', 'Buck', 'Ocho', 'Seabiscuit']
+var colors = ['yellow', 'violet', 'green', 'blue'];
+var horseNames = ['Chip', 'Buck', 'Ocho', 'Seabiscuit'];
+
+colors.push('red');
+horseNames.push('Admiral');
+
+//here's how we're going to loop through each one, using i as an index
+//initializing the object once (outside of the loop)
+var horseColorsAndNames = {};
+
+for (var i=0; i < colors.length; i++) {
+
+	var currentColor = colors[i];
+	var currentName = horseNames[i];
+	
+	horseColorsAndNames[currentName] = currentColor;
+
+}
+
+console.log(horseColorsAndNames);
+
+//driver code
+
+//var horse1 = {name: 'Chip', 'age': 4, gallopsFast: true};
+//horse1.age
+//console.log(horse1['name']);
